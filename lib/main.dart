93f1413afd5074ccabc1fd185525b89777c6b70c
kodzimk <proggamedev@gmail.com>
2025-04-26 19:02:52 +0500

    ending 3 page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:uncharted/API/Keys/openai_api.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'uncharted_app.dart';
 
 void main() {
